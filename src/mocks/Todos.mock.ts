7a8e24ec46cfa36e64a6340312896af8ee87a1bc
Jonnathan <jonzxz12@gmail.com>
2026-03-04 20:13:15 -0600

    small dates adjustment on mock object to avoid type problems

--- src/mocks/Todos.mock.ts
+++ src/mocks/Todos.mock.ts
@@ -7,8 +7,8 @@ export const todos: Todo[] = [
     description:
       'Collect wood to craft your first basic tools and unlock better equipment.',
     remainingTime: 120, // segundos
-    created: new Date(),
-    deadline: new Date(Date.now() + 120 * 1000),
+    created: new Date().toString(),
+    deadline: new Date(Date.now() + 120 * 1000).toString(),
     completed: true
   },
 ]
